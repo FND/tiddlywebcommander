f1fecf610f6dc7954a51e3869f6beae2d870a90b
FND <fnd@fnd-oda.(none)>
2010-09-08 14:46:49 +0200

    first stab at interactive navigation
    required some refactoring (namespace, s/UL/OL/) and the addition of a
    simple notification mechanism
    
    NB: Mocking out AJAX requests for development purposes.
    
    Using spackager (http://pypi.python.org/pypi/spackager) for SPA
    compilation in makefile.

---
+++ Makefile
@@ -0,0 +1,17 @@
+.PHONY: lib spa purge
+
+jquery_version = 1.4.2
+
+lib:
+	curl -o "scripts/jquery.js" \
+		"http://ajax.googleapis.com/ajax/libs/jquery/$(jquery_version)/jquery.min.js"
+	curl -o "scripts/jquery-json.js" \
+		"http://jquery-json.googlecode.com/files/jquery.json-2.2.min.js"
+	curl -o "scripts/chrjs.js" \
+		"http://github.com/tiddlyweb/chrjs/raw/master/main.js"
+
+spa: lib
+	spac index.html
+
+purge:
+	cat .gitignore | while read -r entry; do rm -r $$entry; done || true
--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,89 @@
 (function($) {
 
+var host = "/"; // TODO: calculate
+
+var cmd = tiddlyweb.commander = {
+	addNavColumn: function(items) { // XXX: should be Column class with render method
+		this.resetNavColumns(1);
+		var el = $("<ol />");
+		// TODO: sort
+		$.each(items, function(i, item) {
+			$("<li />").text(item).appendTo(el);
+		});
+		el.appendTo("nav");
+	},
+	resetNavColumns: function(level, callback) {
+		$("nav ul, nav ol").slice(level).remove();
+	},
+	toggleFullscreen: function() { // TODO: argument to avoid hard-coding the last pane
+		var els = $(".pane:visible");
+		if(els.length > 1) {
+			els.slice(0, -1).slideUp(function() {
+				els.eq(-1).css("height", "100%"); // XXX: executed multiple times
+			});
+		} else {
+			$(".pane").slideDown(function() { // XXX: fullscreen pane needs special handling to ensure smooth animation
+				$(this).removeAttr("style");
+			});
+		}
+	},
+	notify: function(msg, type) {
+		var el = $("#notification").text(msg).slideDown("slow").
+			click(function(ev) { $(this).slideUp(); });
+		if(type) {
+			el.addClass(type);
+		}
+	}
+};
+
 $("li").live("click", function(ev) {
 	$(this).not(".disabled").
 		siblings().removeClass("selected").end().
-		toggleClass("selected");
+		addClass("selected");
+});
+
+$("ul:first li").click(function(ev) {
+	var type = $(this).text(); // XXX: brittle (e.g. i18n)
+	var collection = new tiddlyweb.Collection(type, host);
+	var callback = function(data, status, xhr) {
+		cmd.addNavColumn(data);
+	};
+	var errback = function(xhr, error, exc) {
+		var msg = xhr.statusText + ": " + xhr.responseText;
+		cmd.notify(msg, "error");
+	};
+	collection.get(callback, errback);
 });
 
 $("#btnFullscreen").click(function(ev) {
-	toggleFullscreen();
+	cmd.toggleFullscreen();
 });
 
-var toggleFullscreen = function() { // TODO: argument to avoid hard-coding the last pane
-	var els = $(".pane:visible");
-	if(els.length > 1) {
-		els.slice(0, -1).slideUp(function() {
-			els.eq(-1).css("height", "100%"); // XXX: executed multiple times
-		});
+// XXX: DEBUG
+$.ajax = function(options) {
+	var xhr = {};
+	var data;
+
+	var resource = options.url.split("/").pop();
+	switch(resource) {
+		case "bags":
+			data = ["Alpha", "Bravo", "Charlie", "Delta"];
+			break;
+		case "recipes":
+			data = ["Omega"];
+			break;
+		default:
+			break;
+	}
+	if(data) {
+		xhr.status = 200;
+		options.success(data, "success", xhr);
 	} else {
-		$(".pane").slideDown(function() { // XXX: fullscreen pane needs special handling to ensure smooth animation
-			$(this).removeAttr("style");
-		});
+		xhr.statusText = "error";
+		xhr.responseText = resource + " failed";
+		options.error(xhr, "error", {});
 	}
 };
+// XXX: /DEBUG
 
 })(jQuery);
