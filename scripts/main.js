16a0438b9bfa8301ab679ff9f1db2a63754b1c7a
FND <fnd@fnd-oda.(none)>
2010-09-08 15:49:57 +0200

    refactored column handling and initialization
    This should provide more flexibility in the long term.

--- scripts/main.js
+++ scripts/main.js
@@ -2,15 +2,43 @@
 
 var host = "/"; // TODO: calculate
 
+var init = function() {
+	var col = new Column(null, ["recipes", "bags", "users"], function(ev) {
+		var type = $(this).text(); // XXX: brittle (e.g. i18n)
+		var collection = new tiddlyweb.Collection(type, host);
+		var action = function(ev) {
+			var name = $(this).text(); // XXX: brittle (e.g. i18n)
+			cmd.notify(name, "info"); // XXX: DEBUG
+		};
+		var callback = function(data, status, xhr) {
+			cmd.addNavColumn(1, data, action);
+		};
+		var errback = function(xhr, error, exc) {
+			var msg = xhr.statusText + ": " + xhr.responseText;
+			cmd.notify(msg, "error");
+		};
+		collection.get(callback, errback);
+	});
+	col.listType = "ul";
+	col = col.render();
+	$("li:last", col).addClass("disabled"); // XXX: hacky?
+
+	$("li").live("click", function(ev) { // XXX: breaks encapsulation!?
+		$(this).not(".disabled").
+			siblings().removeClass("selected").end().
+			addClass("selected");
+	});
+
+	$("#btnFullscreen").click(function(ev) {
+		cmd.toggleFullscreen();
+	});
+};
+
 var cmd = tiddlyweb.commander = {
-	addNavColumn: function(items) { // XXX: should be Column class with render method
-		this.resetNavColumns(1);
-		var el = $("<ol />");
-		// TODO: sort
-		$.each(items, function(i, item) {
-			$("<li />").text(item).appendTo(el);
-		});
-		el.appendTo("nav");
+	addNavColumn: function(level, items, action) {
+		this.resetNavColumns(level);
+		var col = new Column(null, items, action);
+		col.render();
 	},
 	resetNavColumns: function(level, callback) {
 		$("nav ul, nav ol").slice(level).remove();
@@ -36,28 +64,21 @@ var cmd = tiddlyweb.commander = {
 	}
 };
 
-$("li").live("click", function(ev) {
-	$(this).not(".disabled").
-		siblings().removeClass("selected").end().
-		addClass("selected");
-});
-
-$("ul:first li").click(function(ev) {
-	var type = $(this).text(); // XXX: brittle (e.g. i18n)
-	var collection = new tiddlyweb.Collection(type, host);
-	var callback = function(data, status, xhr) {
-		cmd.addNavColumn(data);
-	};
-	var errback = function(xhr, error, exc) {
-		var msg = xhr.statusText + ": " + xhr.responseText;
-		cmd.notify(msg, "error");
-	};
-	collection.get(callback, errback);
-});
-
-$("#btnFullscreen").click(function(ev) {
-	cmd.toggleFullscreen();
-});
+var Column = function(type, items, onClick) {
+	this.type = type; // XXX: unused?
+	this.items = items; // TODO: sort
+	this.onClick = onClick; // XXX: rename?
+	this.listType = "ol";
+	this.container = $("nav"); // XXX: too vague?!
+};
+Column.prototype.render = function() {
+	var self = this;
+	return $("<" + this.listType + " />").
+		append($.map(this.items, function(item, i) {
+			return $("<li />").text(item).click(self.onClick)[0];
+		})).
+		appendTo(this.container);
+};
 
 // XXX: DEBUG
 $.ajax = function(options) {
@@ -85,5 +106,7 @@ $.ajax = function(options) {
 	}
 };
 // XXX: /DEBUG
+
+init();
 
 })(jQuery);
