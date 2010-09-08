b07425d0ca5ed55725acb716c1abe8cba34a6882
FND <fnd@fnd-oda.(none)>
2010-09-08 10:14:08 +0200

    added fullscreen toggle for entity pane
    because it's fun, because we can - it might even turn out to be useful
    (screw YAGNI on this)
    
    NB: Colors adapted from jQuery UI for now - eventually we need to come
    up with a consistent color theme.

--- scripts/main.js
+++ scripts/main.js
@@ -6,4 +6,21 @@ $("li").live("click", function(ev) {
 		toggleClass("selected");
 });
 
+$("#btnFullscreen").click(function(ev) {
+	toggleFullscreen();
+});
+
+var toggleFullscreen = function() { // TODO: argument to avoid hard-coding the last pane
+	var els = $(".pane:visible");
+	if(els.length > 1) {
+		els.slice(0, -1).slideUp(function() {
+			els.eq(-1).css("height", "100%"); // XXX: executed multiple times
+		});
+	} else {
+		$(".pane").slideDown(function() { // XXX: fullscreen pane needs special handling to ensure smooth animation
+			$(this).removeAttr("style");
+		});
+	}
+};
+
 })(jQuery);
