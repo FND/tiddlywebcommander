1630d7b33ea552bfd610251e0e81ce57ae74576b
FND <fnd@fnd-oda.(none)>
2010-10-05 16:32:22 +0200

    added logic to automatically request enhanced privileges on file URIs

---
+++ scripts/sudo.js
@@ -0,0 +1,18 @@
+var sudo = function(fn) {
+	if(document.location.protocol.indexOf("file") == 0 && window.Components &&
+		window.netscape && window.netscape.security) {
+		window.netscape.security.PrivilegeManager.
+			enablePrivilege("UniversalBrowserRead");
+	}
+	return fn();
+};
+
+// hijack ajax function to provide enhanced privileges
+var ajax = $.ajax;
+$.ajax = function() {
+	var self = this;
+	var args = arguments;
+	return sudo(function() {
+		return ajax.apply(self, args);
+	});
+};
